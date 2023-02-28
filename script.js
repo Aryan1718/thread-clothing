f873d26687c3519823ce95914974aed309d2fdf0
Aryan1718 <57013028+Aryan1718@users.noreply.github.com>
2023-02-28 21:05:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+const hamburger = document.querySelector('.hamburger')
+hamburger.onclick = function(){
+    const navBar = document.querySelector('.nav-bar')
+    navBar.classList.toggle('active')
+}
+
+
+  
